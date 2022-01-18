557e68bba6b95ac72826c8a5c987636a84341aa8
Timdremmen <tim.dremmen@gmail.com>
2022-01-18 09:41:43 +0100

    Random forest, confusion matrix

--- Random_Forest.py
+++ Random_Forest.py
@@ -1,11 +1,10 @@
 from pathlib import Path
-
-import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, classification_report
 from sklearn.model_selection import train_test_split
 
 path = Path("data/houses.csv")
@@ -15,24 +14,33 @@ df = pd.read_csv(path)
 #print(df.)
 columns = ['SalePrice','LotArea', 'YearBuilt', 'YrSold', 'OverallQual', 'OverallCond', 'YearRemodAdd', '1stFlrSF', ]
 X=df[columns]  # Features
-y=df['LotShape']  # Labels
+y=df['CentralAir']  # Labels
+
+
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=1)
 
-clf=RandomForestClassifier(n_estimators=100)
+clf=RandomForestClassifier(random_state=1)
 
 clf.fit(X_train,y_train)
 
 y_pred=clf.predict(X_test)
 
-print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
+tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+print('pres', tp/(fp+tp))
+print('sensiviteit', tp/(fn+tp))
+print('spe', tn/(fp+tn))
 
-feature_imp = pd.Series(clf.feature_importances_,index=columns).sort_values(ascending=False)
-feature_imp
+print(confusion_matrix(y_test, y_pred))
+
+print(classification_report(y_test, y_pred))
+feature_imp = pd.Series(clf.feature_importances_, index=columns).sort_values(ascending=False)
+# feature_imp
 
 sns.barplot(x=feature_imp, y=feature_imp.index)
 # Add labels to your graph
 plt.xlabel('Feature Importance Score')
 plt.ylabel('Features')
 plt.title("Visualizing Important Features")
-plt.show()
+#plt.show()
